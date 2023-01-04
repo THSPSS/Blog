9898cc40dec652169939f1fe30648a867d9e5ad3
THSPSS <111225654+THSPSS@users.noreply.github.com>
2023-01-04 12:59:43 +0900

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL,email
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 ##WTForm
@@ -15,13 +15,13 @@ class CreatePostForm(FlaskForm):
 ##RegisterForm
 class RegisterForm(FlaskForm):
     name = StringField("Name",validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(),email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("SGIN ME UP")
 
 ##LoginForm
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(),email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("LET ME IN")
 
