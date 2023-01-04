95ee6babab20187698bff1dbf063057574e61b5c
THSPSS <111225654+THSPSS@users.noreply.github.com>
2023-01-04 13:13:00 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 import smtplib
+import os
 
 
 my_email = "contentc082@gmail.com"
@@ -18,7 +19,7 @@ password = "bokmqyvrtuxuicnj"
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -40,7 +41,7 @@ gravatar = Gravatar(
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
